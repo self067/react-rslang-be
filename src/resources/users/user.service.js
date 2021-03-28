d850812ca00e39ecdae0ae1ef7153e78c6b5ea1a
self067 <seltor@mail.ru>
2021-03-28 11:49:15 +0300

    feat: add avatar

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -16,7 +16,7 @@ const authenticate = async user => {
 
   const tokens = await tokenService.getTokens(userEntity._id);
 
-  return { ...tokens, userId: userEntity._id, name: userEntity.name };
+  return { ...tokens, userId: userEntity._id, name: userEntity.name, avatar: userEntity.avatar };
 };
 
 const get = id => usersRepo.get(id);
